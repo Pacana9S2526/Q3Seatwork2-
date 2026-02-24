c77b56e9a2ba512f37d1e07de7bd28b2318eca03
Pacana-9S-ay2526 <pacana.kl@obmontessori.edu.ph>
2026-02-24 09:00:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function checkDiscount(){
+    let budget = document.getElementById("budget").value;
+    let disVal = budget * 0.2;
+
+    document.getElementById("discount").innerHTML = ''
+       <div class="container p-5 mt-3 bg-secondary text-dark rounded-3">
+       <h2>RECCOMENDED BRANDS</h2>
+       <button type="button" class="btn btn-light fixed">Acer</button> 
+       <button type="button" class="btn btn-light">Asus</button>
+       <button type="button" class="btn btn-light fixed">Dell</button>
+       <p>Discounts: <span id="discount"></span> </p>
+       </div>
+}
+
+function seeLocation(){
+     
+}
+    
